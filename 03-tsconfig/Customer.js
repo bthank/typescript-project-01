4a4fb90f2677cf358bd9573f016379df36027a07
bthank <bthankachan@yahoo.com>
2021-07-15 21:42:54 -0400

    added tsconfig.json file to control compiler options

---
+++ 03-tsconfig/Customer.js
@@ -0,0 +1,32 @@
+"use strict";
+var Customer = /** @class */ (function () {
+    function Customer(theFirst, theLast) {
+        this._firstName = theFirst;
+        this._lastName = theLast;
+    }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (firstName) {
+            this._firstName = firstName;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (lastName) {
+            this._lastName = lastName;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer;
+}());
+// let's create an instance of the class
+var myCustomer = new Customer("Martin", "Dixon");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
